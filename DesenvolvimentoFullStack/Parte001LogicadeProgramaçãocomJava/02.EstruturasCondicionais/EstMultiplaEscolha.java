7cb687396f677ecc48fbd3f6c2cd03d8c6cde504
JhonnatanWill <jhonnatanwill@gmai.com>
2024-11-18 13:49:26 -0300

    Aula 05

---
+++ DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/02.EstruturasCondicionais/EstMultiplaEscolha.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class EstMultiplaEscolha {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Escolha uma opção:");
+        System.out.println("1 - Cadastrar aluno");
+        System.out.println("2 - Cadastrar notas");
+        System.out.println("3 - Listar alunos e notas");
+
+        int numero = entrada.nextInt();
+
+        switch (numero) {
+            case 1:
+                System.out.println("Vamos cadastrar aluno!");
+                break;
+            case 2:
+                System.out.println("Vamos cadastrar nota!");
+                break;
+            case 3:
+                System.out.println("Vamos listar alunos e notas!");
+                break;
+            default:
+                System.out.println("Escolha uma alternativa válida!");
+                break;
+        }
+    }
+}
