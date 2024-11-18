e98c2ae858dd7357b597ffdea634cdc4718b8513
JhonnatanWill <jhonnatanwill@gmai.com>
2024-11-18 13:38:35 -0300

    Aula 04

---
+++ DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/02.EstruturasCondicionais/EstConAlinhada.java
@@ -0,0 +1,13 @@
+public class EstConAlinhada {
+    public static void main(String[] args) {
+        int nota = 4;
+    
+        if (nota >= 5) {
+            System.out.println("Aprovado!");
+        } else if (nota >= 3 && nota < 5) {
+            System.out.println("Recuperação!");
+        } else {
+            System.out.println("Reprovado!");
+        }
+    }
+}
