994f5cec321d41207f25a3a27e57f65bcb2d91cc
JhonnatanWill <jhonnatanwill@gmai.com>
2024-11-18 14:47:14 -0300

    Aula 03

--- DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/03.EstrudurasdeRepeticao/EstRepDoWhile.java
+++ DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/03.EstrudurasdeRepeticao/EstRepDoWhile.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class EstRepDoWhile {
     @SuppressWarnings("resource")
     public static void main(String[] args) {
-        int opcao = 0;
+        int opcao;
 
-        while (opcao != 99) {
+        do {
             System.out.println("Digite um valor qualquer ou 99 para sair!");
             Scanner entrada = new Scanner(System.in);
-            opcao = entrada.nextInt();   
-        }
+            opcao = entrada.nextInt(); 
+        } while (opcao != 99);
     }
 }
