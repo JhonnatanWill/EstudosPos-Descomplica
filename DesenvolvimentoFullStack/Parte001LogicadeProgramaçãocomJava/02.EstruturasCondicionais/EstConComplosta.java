7c0e7803b93861dfd026f42627278c272476db5f
JhonnatanWill <jhonnatanwill@gmai.com>
2024-11-18 13:23:45 -0300

    Organização, aula 1 e 2.

---
+++ DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/02.EstruturasCondicionais/EstConComplosta.java
@@ -0,0 +1,11 @@
+public class EstConComplosta {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        if (nota >= 5) {
+            System.out.println("Aprovado!");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
---
+++ DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/02.EstruturasCondicionais/EstConSimples.java
@@ -0,0 +1,9 @@
+public class EstConSimples {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        if (nota >= 5) {
+            System.out.println("Aprovado!");
+        }
+    }
+}
