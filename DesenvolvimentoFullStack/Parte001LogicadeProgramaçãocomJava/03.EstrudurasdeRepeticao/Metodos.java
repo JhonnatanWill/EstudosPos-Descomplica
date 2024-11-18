5675563c8e663836b9471f44f67253e5bc959edb
JhonnatanWill <jhonnatanwill@gmai.com>
2024-11-18 15:06:28 -0300

    Aula 05

---
+++ DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/03.EstrudurasdeRepeticao/Metodos.java
@@ -0,0 +1,10 @@
+public class Metodos {
+    public static int somar(int a, int b){
+        return (a+b);
+    }
+
+    public static void main(String[] args) {
+        int total = Metodos.somar(10,50);
+        System.out.println(total);
+    }
+}
