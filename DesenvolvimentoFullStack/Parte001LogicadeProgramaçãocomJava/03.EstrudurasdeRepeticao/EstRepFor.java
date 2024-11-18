de2fec210e70f09fd38cdcf736ac656bdcf37ae1
JhonnatanWill <jhonnatanwill@gmai.com>
2024-11-18 14:43:09 -0300

    Aula 02

---
+++ DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/03.EstrudurasdeRepeticao/EstRepFor.java
@@ -0,0 +1,7 @@
+public class EstRepFor {
+    public static void main(String[] args) {
+        for (int cont=1; cont <= 10; cont++){
+            System.out.println("Oi! " + cont);
+        }
+    }
+}
