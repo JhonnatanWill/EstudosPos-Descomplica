cc1e7f137cbe70116dcaa1eef0947a8b2d9fda4a
JhonnatanWill <jhonnatanwill@gmai.com>
2024-11-18 20:36:45 -0300

    Aula 01

---
+++ DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/04.VetoreseMatrizes/Vetores.java
@@ -0,0 +1,12 @@
+public class Vetores {
+    public static void main(String[] args) {
+        int valor;
+        int[] dados = new int[5];
+
+        dados[2] = 9;
+        dados[3] = 7;
+        dados[0] = 6;
+
+        System.out.println(dados[3]);
+    }
+}
