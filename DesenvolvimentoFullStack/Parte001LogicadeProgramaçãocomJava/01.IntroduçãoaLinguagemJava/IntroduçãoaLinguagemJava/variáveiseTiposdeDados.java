9376993e41eea9efc9af0d20052bc943bbe8d6c3
JhonnatanWill <jhonnatanwill@gmai.com>
2024-11-18 12:26:09 -0300

    Atualizado

---
+++ DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/01.IntroduçãoaLinguagemJava/IntroduçãoaLinguagemJava/variáveiseTiposdeDados.java
@@ -0,0 +1,12 @@
+public class variáveiseTiposdeDados {
+
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        
+        int numero;
+        float saldo;
+        String nome;
+
+        System.out.println("Hello world!");
+    }
+}
