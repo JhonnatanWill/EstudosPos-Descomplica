2bc796a81631718787787a5eca2b34b67a71fca4
JhonnatanWill <jhonnatanwill@gmai.com>
2024-11-19 10:39:10 -0300

    Aula 01

--- DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/05.IntroduçãoaPOO/ConceitosOrientacaoaObjetos.java
+++ DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/05.IntroduçãoaPOO/ConceitosOrientacaoaObjetos.java
@@ -1,5 +1,5 @@
 public class ConceitosOrientacaoaObjetos {
     public static void main(String[] args) {
-        
+        // Foi apenas explicado o que veremos na matéria e como criar uma class.
     }
 }
