7fa7042afe2f9c314d53ff46329df0d7ea25d259
JhonnatanWill <jhonnatanwill@gmai.com>
2024-11-19 10:51:14 -0300

    Aula 02

---
+++ DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/05.IntroduçãoaPOO/ClassesCao.java
@@ -0,0 +1,11 @@
+public class ClassesCao {
+    String nome;
+    String cor;
+    int idade;
+    double peso;
+    
+    public void Anda(){
+        System.out.println("Estou andando...");
+    }
+    
+}
---
+++ DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/05.IntroduçãoaPOO/ClassesMain.java
@@ -0,0 +1,7 @@
+public class ClassesMain {
+    public static void main(String[] args) {
+        ClassesCao cachorro = new ClassesCao();
+        cachorro.nome = "Bobby";
+        cachorro.Anda();
+    }
+}
