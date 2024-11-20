bd78d7544e991653d071c07284fd89b4a43e1412
JhonnatanWill <jhonnatanwill@gmai.com>
2024-11-20 19:20:39 -0300

    Aula 04

--- DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/08.Collections/TreeSetAula.java
+++ DesenvolvimentoFullStack/Parte001LogicadeProgramaçãocomJava/08.Collections/TreeSetAula.java
@@ -1,3 +1,17 @@
+import java.util.TreeSet;
+
 public class TreeSetAula {
-    
+    public static void main(String[] args) {
+        TreeSet<Integer> numeros = new TreeSet<>();
+        numeros.add(8);
+        numeros.add(6);
+        numeros.add(9);
+        numeros.add(1);
+        System.out.println("TreeSet: " + numeros);
+
+        boolean result = numeros.remove(6);
+        System.out.println("Removeu? " + result);
+
+        System.out.println("TreeSet: " + numeros);
+    }
 }
