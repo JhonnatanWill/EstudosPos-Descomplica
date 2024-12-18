24e29ec3aeaa651ba73028edcff3d56934bcaf06
JhonnatanWill <jhonnatanwill@gmai.com>
2024-12-18 10:43:57 -0300

    Aula 01 a 04 - Conceitos JS

---
+++ DesenvolvimentoFullStack/Parte03IntermediaryFront-End/main.js
@@ -0,0 +1,19 @@
+console.log("Olá");
+var a = 10;
+console.log(a);
+var b = 20;
+var total = a + b;
+console.log(total);
+
+var nomePessoa = "Jhonn";
+console.log(nomePessoa);
+
+var var1 = 1,
+    var2 = 2,
+    var3 = 3;
+
+    console.log(var3);
+
+const valor1 = 100;
+valor2 = 200;
+    console.log(valor1);
