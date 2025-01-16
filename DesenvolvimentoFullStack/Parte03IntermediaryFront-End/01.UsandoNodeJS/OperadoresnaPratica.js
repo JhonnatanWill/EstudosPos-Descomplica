2a79b374d8d1e9648501028784d73ab37c99fb2a
JhonnatanWill <jhonnatanwill@gmai.com>
2025-01-16 10:30:05 -0300

    Aula 03 e 04

---
+++ DesenvolvimentoFullStack/Parte03IntermediaryFront-End/01.UsandoNodeJS/OperadoresnaPratica.js
@@ -0,0 +1,38 @@
+var n = 3;
+//n = n * 10;
+n *= 10;
+
+console.log(n);
+
+var x = 10;
+x--
+
+a = 10;
+b = 20;
+c = 60;
+
+console.log(x);
+
+console.log(n > x);
+
+console.log(n <= x && n != c);
+
+console.log(n <= x || n != c);
+
+console.log(!(n <= x && n != c));
+
+// Verificar de a é maior que b e b é divisivel por 2
+
+console.log(a > b && b % 2 == 0);
+
+// Verificar se b é maior igual a a ou b dividido por 2 o resultado é 2
+
+console.log(a <= b || b / 2 == 2);
+
+// Verificar se a é maior que b e b é maior igual a 5
+
+console.log(a > b && 5 <= b);
+
+// Verificar se b é par ou impar usando ternário
+
+resultado = b % 2 == 0 ? "Par" : "Impar";
