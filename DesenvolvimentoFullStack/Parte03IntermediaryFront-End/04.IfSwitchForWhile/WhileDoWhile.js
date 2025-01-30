d544daa86ed475c920d923f95d0bfcd800849b18
JhonnatanWill <jhonnatanwill@gmai.com>
2025-01-30 14:24:25 -0300

    Aula 07 e 08 - While

---
+++ DesenvolvimentoFullStack/Parte03IntermediaryFront-End/04.IfSwitchForWhile/WhileDoWhile.js
@@ -0,0 +1,15 @@
+var contagem = 1;
+
+while (contagem <= 10) {
+    console.log(contagem);
+    contagem++;
+}
+
+var tempo = 1;
+
+do{
+    console.log(tempo);
+    tempo++;
+}while (tempo < 6)
+
+    
