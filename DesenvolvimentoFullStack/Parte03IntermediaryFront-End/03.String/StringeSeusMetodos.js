1164279dfba41dfb3f218444af551c2625788b59
JhonnatanWill <jhonnatanwill@gmai.com>
2025-01-30 10:33:30 -0300

    Aula 01 - String

---
+++ DesenvolvimentoFullStack/Parte03IntermediaryFront-End/03.String/StringeSeusMetodos.js
@@ -0,0 +1,17 @@
+var nome = "Jhonnatan Willian";
+var x = nome.length;
+console.log(x);
+
+resultado = x > 15 ? "Obrigado!" : "Por favor digite seu nome completo!";
+console.log(resultado);
+
+var res = nome.toLocaleUpperCase();
+console.log(res);
+
+var jogo = "Sport vs Outro";
+var posicao = jogo.indexOf("Sport");
+console.log(posicao);
+
+var cortado = jogo.slice(0,5);
+console.log(cortado);
+
