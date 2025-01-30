9bcdef5262dabfae80f79669e27a22585da32669
JhonnatanWill <jhonnatanwill@gmai.com>
2025-01-30 13:46:27 -0300

    Aula 05 e 06 - For

--- DesenvolvimentoFullStack/Parte03IntermediaryFront-End/04.IfSwitchForWhile/For.js
+++ DesenvolvimentoFullStack/Parte03IntermediaryFront-End/04.IfSwitchForWhile/For.js
@@ -1,6 +1,22 @@
-var carro = [{modelo: 'Audio A3', marca: 'Audi', ano: 2020},
-    {modelo: 'Audio A3', marca: 'Audi', ano: 2020}]
+var carros = [
+    {id: 1, modelo: 'Audio A3', marca: 'Audi', ano: 2020},
+    {id: 2, modelo: 'Compass', marca: 'Jeep', ano: 2021}
+]
 
-for (let caracteristica in carro){
-    console.log(carro[caracteristica]);
+for (let caracteristica in carros){
+    console.log(carros[caracteristica]);
 }
+
+/*
+for(var i = 0; i < carros.length; i++){
+    total += carros[i].ano
+}
+
+for(const i in carros){
+    total += carros[i].ano
+}
+
+for(const carro of carros) {
+    total += carro.ano
+}
+*/
