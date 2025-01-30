30f7f2c17c39e02d2f5d6b4250b16868efc8ba65
JhonnatanWill <jhonnatanwill@gmai.com>
2025-01-30 13:35:49 -0300

    Aula 05 e 06 - For

---
+++ DesenvolvimentoFullStack/Parte03IntermediaryFront-End/04.IfSwitchForWhile/For.js
@@ -0,0 +1,6 @@
+var carro = [{modelo: 'Audio A3', marca: 'Audi', ano: 2020},
+    {modelo: 'Audio A3', marca: 'Audi', ano: 2020}]
+
+for (let caracteristica in carro){
+    console.log(carro[caracteristica]);
+}
