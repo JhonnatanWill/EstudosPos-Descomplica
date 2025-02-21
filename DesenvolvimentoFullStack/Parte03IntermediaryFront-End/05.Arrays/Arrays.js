4a217d83b770289c2e3acf0f8235091455cd11e4
JhonnatanWill <jhonnatanwill@gmai.com>
2025-02-21 10:47:51 -0300

    Aula 03 - Arrays

--- DesenvolvimentoFullStack/Parte03IntermediaryFront-End/05.Arrays/Arrays.js
+++ DesenvolvimentoFullStack/Parte03IntermediaryFront-End/05.Arrays/Arrays.js
@@ -2,6 +2,8 @@ var valores = [8,1,7,2,9];
 
 console.log(valores[3]);
 
+console.log("Array invertido: " + valores.reverse());
+
 for (var pos = 0 ; pos < valores.length; pos++){
     console.log("Posição: " + pos + " Valor: " + valores[pos]);
 }
@@ -22,3 +24,23 @@ for (var pos = 0 ; pos < valores.length; pos++){
 var media = soma/valores.length;
 
 console.log(media);
+
+var arr1 = [1,2,3,4,5,6];
+
+console.log(arr1.join('|'));
+
+var retirado = arr1.shift();
+
+console.log(retirado);
+
+console.log(arr1);
+
+console.log("Posição do número 3: " + arr1.indexOf(3));
+
+arr1.push(7);
+
+console.log(arr1);
+
+arr1.pop();
+
+console.log(arr1);
