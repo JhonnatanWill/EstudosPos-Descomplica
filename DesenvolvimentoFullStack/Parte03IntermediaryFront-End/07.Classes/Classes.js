6c4a8f7ef013bcd4e034720df2e46042fa48317a
JhonnatanWill <jhonnatanwill@gmai.com>
2025-03-03 18:35:55 -0300

    Aula 01 e 02 - Classes

---
+++ DesenvolvimentoFullStack/Parte03IntermediaryFront-End/07.Classes/Classes.js
@@ -0,0 +1,19 @@
+class pessoa{
+    
+    constructor(nome,sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    falar(){
+        console.log("Olá " + this.nome);
+    }
+
+    get nomeCompleto(){
+        console.log("Olá " + this.nome + " " + this.sobrenome);
+    }
+}
+
+p1 = new pessoa("Jhonnatan","Willian");
+p1.falar();
+p1.nomeCompleto;
